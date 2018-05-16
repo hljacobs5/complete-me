f6d13961820918118800efbfa3ae5d84e1198fb3
Haley Jacobs <haleyljacobs@gmail.com>
2018-05-16 09:22:44 -0600

    Set up testing files

---
+++ test/Node-test.js
@@ -0,0 +1,3 @@
+import { assert } from 'chai';
+import Node from '../lib/Node';
+
---
+++ test/Trie-test.js
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+import Trie from '../lib/Trie';
+import Node from '../lib/Node';
+
+describe('Trie', () => {
+  it ('should exist', () => {
+    assert.equal(true = true);
+  })
+})
+
