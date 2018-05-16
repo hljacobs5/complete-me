6e27b9e15178013e298775133c57b8d89f5412eb
Haley Jacobs <haleyljacobs@gmail.com>
2018-05-16 16:23:38 -0600

    Testing functionality

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -3,28 +3,52 @@ import Trie from '../lib/Trie';
 import Node from '../lib/Node';
 
 
-describe('Trie', function() {
-  let trie = new Trie();
-  it ('should increase count') 
+describe('trie', function() {
+
+  it('should instatiate trie', function () {
+    let trie = new Trie();
+  });
+
+  it ('should increase count', function () { 
+    let trie = new Trie();
     assert.equal(trie.count, 0);
     trie.insert('word');
     assert.equal(trie.count, 1);
-    console.log(JSON.stringify(trie,null, 4))
+    console.log(JSON.stringify(trie, null, 4))
+  });
 
-})
+  it('should start with a word count of zero', function () {
+    let trie = new Trie();
+    assert(trie.count).to.equal(0);
+  });
+});
 
-  it('should start with a word count of zero', function(){
-    expect(trie.count).to.equal(0);
-  })
+describe('insert', function () {
 
-describe('Insert', function () {
-  let trie = new Trie();
-  trie.insert('doggo');
-  trie.insert('cat');
-  trie.insert('dog');
-  console.log(JSON.stringify(trie,null, 4))
-  expect(trie.count).to.equal(1);
+  it('should insert a new word', () => {
+    let trie = new Trie();
+    trie.insert('dog');
+    trie.insert('cat');
+    trie.insert('doggo');
+    trie.insert('doggy');
+    console.log(JSON.stringify(trie, null, 4))
+  });
+});
 
-})
+describe('suggest', function () {
+  
+  it('should take a prefix', function () {  
+    let trie = new Trie();
+    trie.insert('dog');
+    trie.insert('cat');
+    trie.insert('doggo');
+    trie.insert('doggy');
+    trie.insert('dog')
+    console.log(trie.suggest('do'))
+    console.log(JSON.stringify(trie, null, 4))
+    assert(true).to.equal(true);
+  });
+});
+
 
 
